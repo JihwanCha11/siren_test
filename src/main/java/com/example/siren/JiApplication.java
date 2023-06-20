f6f7167ea25c3f87ba56e2fa1455630135fc3bcf
tjoeun_jg <tjoeun_jg@DESKTOP-D045G56>
2023-06-20 15:39:00 +0900

    굿굿

---
+++ src/main/java/com/example/siren/JiApplication.java
@@ -0,0 +1,13 @@
+package com.example.siren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiApplication.class, args);
+	}
+
+}
